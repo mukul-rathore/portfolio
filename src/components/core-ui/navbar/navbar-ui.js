4e9906a9a1f29957421fa6d67e560eedd6fe9bda
Mukul Rathore <singhmukul329@gmail.com>
2022-12-23 16:41:55 +0530

    Changed Navbar

--- src/components/core-ui/navbar/navbar-ui.js
+++ src/components/core-ui/navbar/navbar-ui.js
@@ -139,7 +139,7 @@ function NavbarUI({ theme, shortname, classes, handleDrawerOpen, handleDrawerClo
                                         className={classes.drawerIcon}
                                     />
                                     <span className={classes.drawerLinks}>
-                                        Blog
+                                        Certifications
                                     </span>
                                 </div>
                             </NavLink>
